af14194e52884f9956c336ee8cecb6204a301575
kpensec <kevin.pensec@gmail.com>
2018-03-28 15:41:47 +0200

    Updating compose configuration and ignite script

--- docker-ignite.sh
+++ docker-ignite.sh
@@ -1,3 +1,5 @@
-docker-compose down
-docker-compose build # TODO should be replaced x-smart-recreate
-docker-compose up -d
+COMPOSE_ARGUMENT='--project-directory . -f docker-compose/docker-compose.yml -f docker-compose/docker-compose-tx.yml'
+
+docker-compose $COMPOSE_ARGUMENT down
+docker-compose $COMPOSE_ARGUMENT build # TODO should be replaced x-smart-recreate
+docker-compose $COMPOSE_ARGUMENT up -d
