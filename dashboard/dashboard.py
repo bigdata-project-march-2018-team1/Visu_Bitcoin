979626de7e3704c76684156c9e7196d8b20a5439
simonetthomas <thomas-tm@hotmail.fr>
2018-03-29 12:55:21 +0200

    better function names

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,6 +3,7 @@ import datetime
  
 width = str(1500)
 height = str(800)
+url="localhost:5601"
 
 # Default route
 @bottle.route("/")
@@ -12,27 +13,27 @@ def index():
 # Show last hour
 @bottle.route("/lasthour")
 @bottle.view("page.tpl")
-def index() :
+def lasthour() :
 	datestart="now-1h"
-	iframe="<iframe src=\"http://localhost:5601/app/kibana#/dashboard/cb2e3d00-2c49-11e8-b68c-239c34f5932e?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A202'%2Cdisplay%3A'5%20seconds'%2Cpause%3A!f%2Csection%3A1%2Cvalue%3A5000)%2Ctime%3A(from%3A"+datestart+"%2Cmode%3Aquick%2Cto%3Anow))\" height="+height+" width="+width+"></iframe>"
+	iframe="<iframe src=\"http://"+url+"/app/kibana#/dashboard/cb2e3d00-2c49-11e8-b68c-239c34f5932e?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A202'%2Cdisplay%3A'5%20seconds'%2Cpause%3A!f%2Csection%3A1%2Cvalue%3A5000)%2Ctime%3A(from%3A"+datestart+"%2Cmode%3Aquick%2Cto%3Anow))\" height="+height+" width="+width+"></iframe>"
 	return {"datestart":datestart, "dateend":"now", "iframe":iframe }
 	
 # Show x last days
 @bottle.route("/lastdays/<days:int>")
 @bottle.view("page.tpl")
-def index(days) :
+def lastdays(days) :
 	datestart="now-"+str(days)+"d"
-	iframe="<iframe src=\"http://localhost:5601/app/kibana#/dashboard/cb2e3d00-2c49-11e8-b68c-239c34f5932e?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A202'%2Cdisplay%3A'5%20seconds'%2Cpause%3A!f%2Csection%3A1%2Cvalue%3A5000)%2Ctime%3A(from%3A"+datestart+"%2Cmode%3Aquick%2Cto%3Anow))\" height=\"700\" width=\"1500\"></iframe>"
+	iframe="<iframe src=\"http://"+url+"/app/kibana#/dashboard/cb2e3d00-2c49-11e8-b68c-239c34f5932e?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A202'%2Cdisplay%3A'5%20seconds'%2Cpause%3A!f%2Csection%3A1%2Cvalue%3A5000)%2Ctime%3A(from%3A"+datestart+"%2Cmode%3Aquick%2Cto%3Anow))\" height="+height+" width="+width+"></iframe>"
 	return {"datestart":datestart, "dateend":"now", "iframe":iframe }
 
  
 # Show from date to date
 @bottle.post('/fromdatetodate')
 @bottle.view("page.tpl")
-def index():
+def fromdatetodate():
 	datestart = bottle.request.forms.get('datestart')
 	dateend = bottle.request.forms.get('dateend')
-	iframe="<iframe src=\"http://localhost:5601/app/kibana#/dashboard/cb2e3d00-2c49-11e8-b68c-239c34f5932e?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A202'%2Cdisplay%3A'5%20seconds'%2Cpause%3A!f%2Csection%3A1%2Cvalue%3A5000)%2Ctime%3A(from%3A'"+datestart+"'%2Cmode%3Aabsolute%2Cto%3A'2018-03-28T21%3A59%3A59.999Z'))\" height=\"700\" width=\"1500\"></iframe>"
+	iframe="<iframe src=\"http://"+url+"/app/kibana#/dashboard/cb2e3d00-2c49-11e8-b68c-239c34f5932e?embed=true&_g=(refreshInterval%3A('%24%24hashKey'%3A'object%3A202'%2Cdisplay%3A'5%20seconds'%2Cpause%3A!f%2Csection%3A1%2Cvalue%3A5000)%2Ctime%3A(from%3A'"+datestart+"'%2Cmode%3Aabsolute%2Cto%3A'2018-03-28T21%3A59%3A59.999Z'))\" height="+height+" width="+width+"></iframe>"
 	return {"datestart":datestart, "dateend":dateend, "iframe":iframe }
 
 @bottle.route('/static/<filename:path>')
